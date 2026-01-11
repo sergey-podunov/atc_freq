c4484ccd047da4a99f506d138287f605ae13ad19
Sergei Podunov <podunov.sergey@gmail.com>
2026-01-11 15:47:30 +0100

    add cli args parser

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -4,32 +4,98 @@ package main
 import (
 	"atc_freq/internal/sim"
 	"fmt"
+	"os"
+	"strings"
 	"time"
+
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
+	app := &cli.App{
+		Name:  "atc_freq",
+		Usage: "Get airport frequencies and weather information from MSFS",
+		Commands: []*cli.Command{
+			{
+				Name:      "freq",
+				Usage:     "Get all frequencies for an airfield",
+				ArgsUsage: "<ICAO>",
+				Action:    freqCommand,
+				Description: "Retrieves and displays all available frequencies for the specified airport.\n\n   Example:\n      atc_freq freq EDDB",
+			},
+			{
+				Name:      "weather",
+				Usage:     "Get weather at waypoints",
+				ArgsUsage: "<waypoint1,waypoint2,...>",
+				Action:    weatherCommand,
+				Description: "Retrieves weather information for a comma-separated list of waypoints.\n\n   Example:\n      atc_freq weather EDDB,UUMI,KJFK",
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func freqCommand(ctx *cli.Context) error {
+	if ctx.NArg() != 1 {
+		return fmt.Errorf("requires exactly one ICAO code argument")
+	}
+
+	icao := ctx.Args().Get(0)
+
 	client, err := sim.NewSimClient()
 	if err != nil {
-		fmt.Println("Failed to load SimConnection:", err)
-		return
+		return fmt.Errorf("failed to load SimConnection: %w", err)
 	}
-	// Now you can reuse 'client' for multiple calls
-	freqs, err := client.GetAirportFrequencies("EDDB", 10*time.Second)
-	if err != nil {
-		fmt.Println("ERR:", err)
-		return
-	}
-	for _, f := range freqs {
-		fmt.Printf("%-10s %8.3f  %s\n", f.Type, f.MHz, f.Name)
-	}
-	fmt.Println()
 
-	freqs, err = client.GetAirportFrequencies("UUMI", 10*time.Second)
+	freqs, err := client.GetAirportFrequencies(icao, 10*time.Second)
 	if err != nil {
-		fmt.Println("ERR:", err)
-		return
+		return fmt.Errorf("failed to get frequencies: %w", err)
 	}
+
+	if len(freqs) == 0 {
+		fmt.Printf("No frequencies found for %s\n", icao)
+		return nil
+	}
+
+	fmt.Printf("Frequencies for %s:\n", strings.ToUpper(icao))
 	for _, f := range freqs {
-		fmt.Printf("%-10s %8.3f  %s\n", f.Type, f.MHz, f.Name)
+		fmt.Printf("  %-10s %8.3f MHz  %s\n", f.Type, f.MHz, f.Name)
 	}
+
+	return nil
+}
+
+func weatherCommand(ctx *cli.Context) error {
+	if ctx.NArg() != 1 {
+		return fmt.Errorf("requires exactly one argument (comma-separated waypoints)")
+	}
+
+	waypointsStr := ctx.Args().Get(0)
+	waypoints := strings.Split(waypointsStr, ",")
+
+	// Trim whitespace from each waypoint
+	for i := range waypoints {
+		waypoints[i] = strings.TrimSpace(waypoints[i])
+	}
+
+	if len(waypoints) == 0 {
+		return fmt.Errorf("no waypoints provided")
+	}
+
+	fmt.Println("Weather command - to be implemented")
+	fmt.Printf("Requested waypoints: %v\n", waypoints)
+
+	// TODO: Implement weather retrieval logic
+	// For now, just show what waypoints were requested
+	for _, wp := range waypoints {
+		if wp != "" {
+			fmt.Printf("  - %s: (weather data not yet implemented)\n", strings.ToUpper(wp))
+		}
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -3,12 +3,14 @@ module atc_freq
 go 1.25.5
 
 require (
+	github.com/urfave/cli/v2 v2.27.5
 	github.com/wailsapp/wails/v2 v2.11.0
 	golang.org/x/sys v0.30.0
 )
 
 require (
 	github.com/bep/debounce v1.2.1 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.5 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
@@ -25,12 +27,14 @@ require (
 	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/samber/lo v1.49.1 // indirect
 	github.com/tkrajina/go-reflector v0.5.8 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	github.com/wailsapp/go-webview2 v1.0.22 // indirect
 	github.com/wailsapp/mimetype v1.4.1 // indirect
+	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
 	golang.org/x/crypto v0.33.0 // indirect
 	golang.org/x/net v0.35.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
